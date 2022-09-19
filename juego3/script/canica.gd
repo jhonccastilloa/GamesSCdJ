16454f5441ff7d88a30820dd7ce521769fb8b7ab
Jhon Castillo <gpro1pro@gmail.com>
2022-09-18 23:04:05 -0500

    game 2 modified

--- juego3/script/canica.gd
+++ juego3/script/canica.gd
@@ -10,13 +10,32 @@ extends RigidBody2D
 func _ready():
 	set_bounce(0.5)
 	set_friction(0.2)
+#	self.mode=1
 	
+#	position.x=250
+#	print(position.x)
 	pass # Replace with function body.
 
+func _process(delta):
+	print(mode)
+	pass
+	
 func launch(force : Vector2):
+	self.mode=0
+	
 	apply_impulse(Vector2.ZERO,force)
+#	print(apply_impulse(Vector2.ZERO,force))
+#	get_parent().get_node("canica").mode=3
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 
+
+
+func _on_canica_sleeping_state_changed():
+#	self.mode=1
+	print("dormido")
+	
+	pass # Replace with function body.
--- juego3/script/vector_lanzar.gd
+++ juego3/script/vector_lanzar.gd
@@ -3,7 +3,6 @@ extends Area2D
 signal vector_created(vector)
 
 export var maximum_length := 200
-
 var touch_down := false
 var position_start := Vector2.ZERO
 var position_end := Vector2.ZERO
@@ -17,8 +16,18 @@ var vector := Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready():
 #	_draw()
+#	get_parent().get_node("canica").position=Vector2(250,250)
 #	connect("input_event",self,"_on_input_event")
 	pass # Replace with function body.
+	
+func _physics_process(delta):
+	if Input.is_action_pressed("left_click"):
+		get_parent().get_node("canica").global_position.x=position_start.x
+		get_parent().get_node("canica").global_position.y=position_start.y
+#		get_parent().get_node("canica").global_position=Vector2(position_start)
+
+		
+		
 func _reset():
 	var position_start = Vector2.ZERO
 	var position_end = Vector2.ZERO
@@ -28,13 +37,9 @@ func _reset():
 func _draw()->void:
 	draw_line(position_start-global_position,
 		(position_end-global_position),
-		Color.blue,
-		8)
+		Color.blue,8)
 	draw_line(position_start-global_position,(position_start-global_position)+vector,
-		Color.red,
-		18)
-	
-	
+		Color.red,18)
 func _input(event)->void:
 	if not touch_down:
 		return
@@ -45,14 +50,17 @@ func _input(event)->void:
 		
 	if event is InputEventMouseMotion:
 		position_end=event.position
-#		print(position_end)
 		vector=-(position_end-position_start).clamped(maximum_length)
 		print(vector)
 		update()
 
 func _on_vector_lanzar_input_event(viewport, event, shape_idx):
 	if Input.is_action_pressed("left_click"):
+		get_parent().get_node("canica").mode=1
+		
 		touch_down=true
 		position_start=event.position
 		print(position_start)
 	pass # Replace with function body.
+
+
