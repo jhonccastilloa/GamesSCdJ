f2e59cf3c123adea62b0854b185ab349a8016182
Jhon Castillo <gpro1pro@gmail.com>
2022-09-21 09:10:42 -0500

    created game 4

---
+++ juego4/scenes/HUD.gd
@@ -0,0 +1,41 @@
+extends CanvasLayer
+
+signal left_pressed
+signal rigth_pressed
+signal up_pressed
+signal down_pressed
+
+
+signal left_released
+signal rigth_released
+signal up_released
+signal down_released
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_left_pressed():
+	emit_signal("left_pressed")
+func _on_rigth_pressed():
+	emit_signal("rigth_pressed")
+func _on_up_pressed():
+	emit_signal("up_pressed")
+func _on_down_pressed():
+	emit_signal("down_pressed")
+
+
+
+func _on_left_released():
+	pass # Replace with function body.
---
+++ juego4/script/kite.gd
@@ -0,0 +1,44 @@
+extends RigidBody2D
+
+export var speed = 400
+var velocity = Vector2.ZERO
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var press=false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$ap_kite.play("fly")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_HUD_down_pressed():
+	velocity.y += 1
+	press=true
+	print(velocity)
+func _on_HUD_left_pressed():
+	velocity.x -= 1
+	press=true
+	
+	print(velocity)
+
+func _on_HUD_rigth_pressed():
+	velocity.x += 1
+	press=true
+	
+	print(velocity)
+
+func _on_HUD_up_pressed():
+	velocity.y -= 1
+	press=true
+	
+	print(velocity)
+	
+func _physics_process(delta):
+	print(press)
