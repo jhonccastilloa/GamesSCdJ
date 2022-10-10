5adb710a1b8e6cb7b4ecc9145c64b53613ff428e
Castillo <gpro1pro@gmail.com>
2022-10-10 17:43:50 -0500

    game 1 new version

---
+++ juego1/environment/menu_win.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+signal resume
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_btn_restart_pressed():
+	get_tree().paused=false
+	get_tree().change_scene("res://juego1/world.tscn")
+	pass # Replace with function body.
+
+
+func _on_btn_salir_pressed():
+	get_tree().paused=false
+	get_tree().change_scene("res://Menu.tscn")
+	pass # Replace with function body.
--- juego1/world_new.gd
+++ juego1/world_new.gd
@@ -3,15 +3,18 @@ extends Node2D
 var ovejaEli = 0
 var num_win=0
 var scene_pause = load("res://menu_pause.tscn")
+var scene_win = load("res://juego1/environment/menu_win.tscn")
+
 var pause=false
 func _ready():
+	print(scene_win)
 	pass
 
 func ovejasMuertas():
 	ovejaEli+=1
 	get_node("InterfazDeUsuario/VBoxContainer/ovejasComidas").text="0"+str(ovejaEli)
 	if(ovejaEli==6):
-		get_tree().change_scene("res://juego1/ganoZorro.tscn")
+		win("EL ZORRO SE COMIO A LAS OVEJAS")
 	
 	print(ovejaEli)
 
@@ -20,8 +23,8 @@ func _on_LineaGanar_area_entered(area):
 	num_win+=1
 	print(num_win)
 	if num_win==6:
-		get_tree().change_scene("res://juego1/ganoOveja.tscn")
-		print("ganaron las ovejas") # Replace with function body.
+		win("GANARON LAS OVEJAS")
+		
 
 
 func _on_btn_pause_pressed():
@@ -34,3 +37,12 @@ func _on_btn_pause_pressed():
 func on_restart():
 	get_tree().paused=0
 	get_tree().change_scene("res://juego1/world.tscn")
+
+func win(text):
+	var scn_win=scene_win.instance()
+	$"Screen_win".add_child(scn_win)
+	var lbl_information=$"Screen_win/menu_win/HBoxContainer/lbl_information"
+	lbl_information.text=text
+	lbl_information.align=1
+	get_tree().paused=1
+	
